fc17109141f18787203282105ce094f21ec01e60
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-12 16:26:14 +0530

    Create BookController and UserController

---
+++ src/main/java/com/sabir/springboot/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.sabir.springboot.controllers;
+
+import com.sabir.springboot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+}
