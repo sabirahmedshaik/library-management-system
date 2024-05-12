4a96ddad68a9dd5c4e3e75947b6e347885f4e5dc
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-12 14:28:57 +0530

    Create Book and User Entities

---
+++ src/main/java/com/sabir/springboot/entities/User.java
@@ -0,0 +1,23 @@
+package com.sabir.springboot.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+}
