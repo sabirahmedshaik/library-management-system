dffa5eefb630118d8ce9d286eba58a38c4a9bc66
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-12 20:29:33 +0530

    Implement, Build and Test Get Users REST API using Postman Client

--- src/main/java/com/sabir/springboot/services/UserService.java
+++ src/main/java/com/sabir/springboot/services/UserService.java
@@ -2,7 +2,10 @@ package com.sabir.springboot.services;
 
 import com.sabir.springboot.dto.UserDto;
 
+import java.util.List;
+
 public interface UserService {
     UserDto createUser(UserDto userDto);
+    List<UserDto> getUsers();
 
 }
