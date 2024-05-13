e10aceb701f35a49558ace4e209c0db49123fd29
sabirahmedshaik <sabirahmed.piya@gmail.com>
2024-05-14 00:03:35 +0530

    Implement, Build and Test Return Book REST API using Postman Client

--- src/main/java/com/sabir/springboot/services/BookService.java
+++ src/main/java/com/sabir/springboot/services/BookService.java
@@ -5,11 +5,13 @@ import com.sabir.springboot.dto.BookDto;
 import java.util.List;
 
 public interface BookService {
+
     BookDto createBook(BookDto bookDto);
     List<BookDto> getBooks();
     BookDto getBookById(Long bookId);
     BookDto updateBook(Long book_id, BookDto book);
     void deleteBook(Long book_id);
     BookDto borrowBook(Long book_id, Long user_id);
+    BookDto returnBook(Long book_id);
 
 }
